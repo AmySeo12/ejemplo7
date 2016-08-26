5d81571ccd16a0f38ffb7e40b799fe929b5f8ddc
AmySeo12 <maryori.amy@gmail.com>
2016-08-26 10:49:08 -0500

    subiendo ejemplo 7

---
+++ app.js
@@ -0,0 +1,15 @@
+var n = parseInt(prompt("Ingrese un numero"));
+	if((n%2 == 0 ) && n>20){
+		window.alert("Perfect!");
+	}
+	else if(n %2 == 0 &&  n >= 2 &&  n <5){
+    window.alert("Good!");
+	}
+	else if(numero %2 ==0 && numero >= 6 && numero <= 20){
+    window.alert("Great!");
+	}
+	else {
+    window.alert("Odd");
+	}
+
+
